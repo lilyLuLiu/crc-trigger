6fc508c5f24305b9e3839414f780ba3ef214b9f8
lilyLuLiu <lilyliulu777@outlook.com>
2024-11-12 14:25:44 +0800

    update

--- trigger-bundle-tes.sh
+++ trigger-bundle-tes.sh
@@ -13,16 +13,26 @@ fi
 cp -r bundle-test tmp
 
 # modify pipeline run file
-sed -i'' -e "s#<B_VERSION>#$1#g" tmp/rhel1-x86.yaml
-sed -i'' -e "s#<B_VERSION>#$1#g" tmp/mac2-amd64.yaml
-sed -i'' -e "s#<B_VERSION>#$1#g" tmp/mac4-arm64.yaml
-sed -i'' -e "s#<B_VERSION>#$1#g" tmp/windows-x86.yaml
+sed -i'' -e "s#<B_VERSION>#$1#g" tmp/ocp/rhel1-x86.yaml
+sed -i'' -e "s#<B_VERSION>#$1#g" tmp/ocp/mac2-amd64.yaml
+sed -i'' -e "s#<B_VERSION>#$1#g" tmp/ocp/mac4-arm64.yaml
+sed -i'' -e "s#<B_VERSION>#$1#g" tmp/ocp/windows-x86.yaml
+
+sed -i'' -e "s#<BUNDLE_VERSION>#$1#g" tmp/microshift/rhel.yaml
+sed -i'' -e "s#<BUNDLE_VERSION>#$1#g" tmp/microshift/mac-x86.yaml
+sed -i'' -e "s#<BUNDLE_VERSION>#$1#g" tmp/microshift/mac-arm64.yaml
+sed -i'' -e "s#<BUNDLE_VERSION>#$1#g" tmp/microshift/windows.yaml
 
 # confirm login the correct openshift workspace
 oc project | grep "devtoolsqe--pipeline"
 # create pipeline with yaml file
-oc create -f tmp/rhel1-x86.yaml
-oc create -f tmp/windows-x86.yaml
-oc create -f tmp/mac2-amd64.yaml
-sleep 10*60*60 #two mac pipeline can't run together as they use same machine to build
-oc create -f tmp/mac4-arm64.yaml
+#oc create -f tmp/ocp/rhel1-x86.yaml
+#oc create -f tmp/ocp/windows-x86.yaml
+#oc create -f tmp/ocp/mac2-amd64.yaml
+#sleep 10*60*60 #two mac pipeline can't run together as they use same machine to build
+#oc create -f tmp/ocp/mac4-arm64.yaml
+
+#oc create -f tmp/microshift/rhel.yaml
+#oc create -f tmp/microshift/mac-x86.yaml
+#oc create -f tmp/microshift/mac-arm64.yaml
+#oc create -f tmp/microshift/windows.yaml
