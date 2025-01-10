e1784a01b7737549aa3a5ec078f4e9be0853c7ef
Alberto Fanjul <albertofanjul@gmail.com>
2025-01-10 10:52:59 +0100

    Add bundle test for linux arm64

--- trigger-bundle-test.sh
+++ trigger-bundle-test.sh
@@ -31,12 +31,15 @@ sed -i'' -e "s#<BUNDLE_VERSION>#$1#g" tmp/microshift/windows.yaml
 oc project | grep "devtoolsqe--pipeline"
 # create pipeline with yaml file
 #oc create -f tmp/ocp/rhel1-x86.yaml
+#oc create -f tmp/ocp/rhel-arm64.yaml
 #oc create -f tmp/ocp/windows-x86.yaml
 #oc create -f tmp/ocp/mac2-amd64.yaml
 #sleep 10*60*60 #two mac pipeline can't run together as they use same machine to build
 #oc create -f tmp/ocp/mac4-arm64.yaml
 
 #oc create -f tmp/microshift/rhel.yaml
+#oc create -f tmp/microshift/rhel-arm64.yaml
 #oc create -f tmp/microshift/mac-x86.yaml
-#oc create -f tmp/microshift/mac-arm64.yaml
 #oc create -f tmp/microshift/windows.yaml
+#sleep 10*60*60 #two mac pipeline can't run together as they use same machine to build
+#oc create -f tmp/microshift/mac-arm64.yaml
